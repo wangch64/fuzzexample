9a9d16c8d9903a4dbb40def984efa754dbec5461
one <wangch64@gmail.com>
2023-09-21 13:17:41 +0800

    commit

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,15 +1,7 @@
 #!/bin/bash -eu
 
-./buildconf.sh
-# configure scripts usually use correct environment variables.
-./configure
-
-make clean
-make -j$(nproc) all
 
 $CXX $CXXFLAGS -std=c++11 -Ilib/ \
-    $SRC/parse_fuzzer.cc -o $OUT/parse_fuzzer \
+    $SRC/example/main.cpp -o $OUT/parse_fuzzer \
     $LIB_FUZZING_ENGINE .libs/libexpat.a
 
-# Optional: Copy dictionaries and options files.
-cp $SRC/*.dict $SRC/*.options $OUT/
