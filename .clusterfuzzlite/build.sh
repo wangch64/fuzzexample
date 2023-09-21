0e2f51b7542435be3b4c3ad8e96c4029669bdc56
one <wangch64@gmail.com>
2023-09-22 00:47:07 +0800

    commit

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -eu
 
 $CXX $CXXFLAGS -std=c++11 -Ilib/ \
-    $SRC/parse_fuzzer.cc -o $OUT/parse_fuzzer \
+    $SRC/example/main.cpp -o $OUT/ex1 \
     $LIB_FUZZING_ENGINE /usr/lib/libFuzzingEngine.a
